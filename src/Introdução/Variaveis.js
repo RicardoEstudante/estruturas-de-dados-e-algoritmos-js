98b3ec141ced06f49504d11c04cd0f0a8005ba72
Ricardo Carvalho Santos <42756551+RicardoEstudante@users.noreply.github.com>
2020-07-21 12:34:32 -0300

    add operadores

---
+++ src/Introdução/Variaveis.js
@@ -0,0 +1,59 @@
+/* palavras reservadas usadas para declarar variaveis */
+
+// var
+// let
+// const
+
+/*  Tipos de dados */
+// Number
+// String
+// Boolean
+// Object
+// Undefined
+// Null
+// Function
+
+/* Exemplos */
+
+var num       = 1; // number
+num2          = 2; // var não é obrigatório
+var price     = 1.5; // number decimal
+var myName    = 'Packt'; // string
+var trueValue = true; // boolean
+var nullVAr   = null; // null
+var undefined; // undefined;
+
+// o Valor null quer dizer sem valor,
+// o valor undefined quer dizer ainda não recebeu valor.
+
+
+
+/* Espoco das variáveis */
+
+// Escopo refere-se ao local em que podemos acessar a variavel no algoritmo,
+// variaveis podem ser Globais ou Locais.
+
+var myVariable = 'global';
+var myOtherVariable = 'global';
+
+function myFunction() {
+  var myVariable = 'local';
+  return myVariable;
+}
+
+function myOtherFunction() {
+  myOtherVariable = 'local';
+  return myOtherVariable;
+}
+
+console.log(myVariable); // Global pois foi declarada no escopo global.
+console.log(myFunction()); // Local pois foi declarada dentro da função.
+console.log(myOtherVariable); // Global pois foi declarada no escopo global.
+console.log(myOtherFunction()); // Local pois não usamos var e referenciamos uma variavel existente no escopo global e mudamos o valor dela.
+console.log(myOtherVariable); // Local pois usamos a função para mudar o valor da variavel de modo global.
+
+// Evite usar variaveis Globais
+
+
+
+
